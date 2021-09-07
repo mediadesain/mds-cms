136b2a8d2c1caa172f99e081ebde4a6abb8b0500
Reynaldi Julio Setiabudi <reynaldi.setiabudi@jublia.com>
2021-09-08 01:30:17 +0700

    FEATURE(search, filter) : Search & Filter added

--- src/app/shared/pipes/_pipes.module.ts
+++ src/app/shared/pipes/_pipes.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FilterPipe } from './filter.pipe';
 import { SafeUrlPipe } from './safe-url.pipe';
+import { SearchPipe } from './search.pipe';
 
 @NgModule({
   declarations: [
     FilterPipe,
-    SafeUrlPipe
+    SafeUrlPipe,
+    SearchPipe
   ],
   imports: [CommonModule],
   exports: [
     FilterPipe,
-    SafeUrlPipe
+    SafeUrlPipe,
+    SearchPipe
   ]
 })
 export class PipesModule {}
